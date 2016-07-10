9d778e2e7a268d7145c4789b3e42a33b3a69d7be
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-07-10 15:05:07 +0545

    prototype done

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ module.exports = function(app, passport){
 		Post.find({}, function(err, result){
 			if (err) {
 				res.render('index.ejs', { post: ''});
-				throw err;
+				//throw err;
 			}
 			res.render('index.ejs', { post : result });
 		});
