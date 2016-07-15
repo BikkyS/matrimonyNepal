c38fad187377564e07d2d6f6ba12c822228bca2c
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-07-15 22:36:37 +0545

    test case added

--- test/test-routes-index.js
+++ test/test-routes-index.js
@@ -4,11 +4,48 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
 
-var server = ('../bin/www');
-var User = require('../models/user');
-
+chai.config.includeStack = true;
 var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('User')
+var server = ('../app');
+var User = require('../models/user');
+var Post = require('../models/post');
+
+describe('Routes before logging in', function(){
+
+	Post.collection.drop();
+
+	beforeEach(function(done){
+		var newPost = new Post({
+			_id: 12345,
+			name: 'John',
+			gender: 'Male',
+			age: 26,
+			email: 'john@john.com',
+			phone: 9849098490,
+			qualification: 'ME',
+			lookingFor: 'Hot lady',
+			bio: 'Hello from NYC'
+		});
+		newPost.save(function(err){
+			done();
+		});
+	});
+
+	afterEach(function(done){
+		Post.collection.drop();
+		done();
+	});
+
+	it('should list ALL posts on / GET', function(done){
+		chai.request(server)
+			.get('/')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			});
+	});
+
+});
