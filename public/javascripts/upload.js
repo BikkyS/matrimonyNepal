807b48e1ac6be5030d9eb2425ff0db9045a6945c
BikkyS <72158443+BikkyS@users.noreply.github.com>
2020-09-30 23:46:50 +0530

    Update upload.js

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -10,7 +10,7 @@ $('#upload-input').on('change', function(){
   var files = $(this).get(0).files;
 
   if (files.length > 0){
-    // create a FormData object which will be sent as the data payload in the
+    // Create a FormData object which will be sent as the data payload in the
     // AJAX request
     var formData = new FormData();
 
@@ -18,7 +18,7 @@ $('#upload-input').on('change', function(){
     for (var i = 0; i < files.length; i++) {
       var file = files[i];
 
-      // add the files to formData object for the data payload
+      // Add the files to formData object for the data payload
       formData.append('uploads[]', file, file.name);
     }
 
